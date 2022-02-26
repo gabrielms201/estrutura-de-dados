640ff7f0afb2217afd6cf92ea6055436862932e6
Ricardo Ruiz <ricardo.ruiz@intelitrader.com.br>
2022-02-25 22:49:49 -0300

    Finaliza amostra da piha

--- Estrutura-De-Dados-P1/Estrutura-De-Dados-P1/Programa.cpp
+++ Estrutura-De-Dados-P1/Estrutura-De-Dados-P1/Programa.cpp
@@ -1,5 +1,26 @@
 #include "Programa.h"
+void sample()
+{
+	Pilha pilha = Pilha();
+	pilha.Init();
+	// Deve popular a pilha
+	pilha.Push(10);
+	pilha.Push(9);
+	pilha.Push(8);
+	pilha.Push(7);
+	pilha.Push(6);
+	pilha.Push(5);
+	pilha.Push(4);
+	pilha.Push(3);
+	pilha.Push(2);
+	pilha.Push(1);
+	// Deve remover o ultimo elemento da pilha
+	pilha.Pop();
+	// Deve colocar o numero "2" como último elemento da pilha:
+	pilha.Push(2);
+	pilha.Print();
+}
 int main() 
 {
-
+	sample();
 }
