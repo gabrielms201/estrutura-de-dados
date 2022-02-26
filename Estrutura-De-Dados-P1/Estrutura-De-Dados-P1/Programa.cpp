f43de253c7b0f9c2ae6de1c454505cb4a62c846d
Ricardo Ruiz <ricardo.ruiz@intelitrader.com.br>
2022-02-25 23:08:47 -0300

    Aprimora amostra

--- Estrutura-De-Dados-P1/Estrutura-De-Dados-P1/Programa.cpp
+++ Estrutura-De-Dados-P1/Estrutura-De-Dados-P1/Programa.cpp
@@ -16,9 +16,23 @@ void sample()
 	pilha.Push(1);
 	// Deve remover o ultimo elemento da pilha
 	pilha.Pop();
+	cout << "Apos o primeiro pop: " << endl;
+	cout << "\tTamanho: " << pilha.Size() << endl;
 	// Deve colocar o numero "2" como último elemento da pilha:
 	pilha.Push(2);
+	//
+	cout << "Apos o push do valor 2: " << endl;
+	cout << "\tIsEmpty: " << pilha.IsEmpty() << endl;
+	cout << "\tIsFull: " << pilha.IsFull() << endl;
+	cout << "\tTamanho: " << pilha.Size() << endl;
+	cout << "Valor da Pilha: ";
 	pilha.Print();
+	for (int i = 0; i < 10; i++)
+	{
+		pilha.Pop();
+	}
+	cout << "\nApos esvaziar a pilha: " << endl;
+	cout << "\tIsEmpty: " << pilha.IsEmpty() << endl;
 }
 int main() 
 {
