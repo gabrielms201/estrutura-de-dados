d3e4db4e556b36f489b5dabfe8e04d6e4aca6ad3
Ricardo Ruiz <ricardo.ruiz@intelitrader.com.br>
2022-03-01 15:05:36 -0300

    Coloca comentarios do grupo no header de erros

--- Estrutura-De-Dados-P1/src/Errors.h
+++ Estrutura-De-Dados-P1/src/Errors.h
@@ -1,3 +1,11 @@
+/*
+P1 - Estrutura de Dados
+Grupo:
+Brian Diniz Bracco						| TIA: 41517687
+Mayara Meneghetti Honda					| TIA: 32152280
+Paulo Henrique Braga Cechinel			| TIA: 32151128
+Ricardo Gabriel Marques dos Santos Ruiz | TIA: 32134908
+*/
 #ifndef ERRORS_H
 #define ERRORS_H
 
