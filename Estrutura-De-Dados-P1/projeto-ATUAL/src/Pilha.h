ee0db6a4753116817eec4e65f90ff806cac10c9c
brianbracco <100450308+brianbracco@users.noreply.github.com>
2022-03-28 17:23:48 -0300

    Update Pilha.h

--- Estrutura-De-Dados-P1/projeto-ATUAL/src/Pilha.h
+++ Estrutura-De-Dados-P1/projeto-ATUAL/src/Pilha.h
@@ -8,6 +8,7 @@ Mayara Meneghetti Honda					| TIA: 32152280
 Paulo Henrique Braga Cechinel			| TIA: 32151128
 Ricardo Gabriel Marques dos Santos Ruiz | TIA: 32134908
 */
+
 #include <iostream>
 
 using namespace std;
@@ -16,8 +17,7 @@ typedef int Elem;
 
 const int TAM_PILHA = 100;
 
-class Pilha 
-{
+class Pilha {
 	private:
    		int topoPilha;
    		Elem e[TAM_PILHA];
@@ -31,6 +31,58 @@ class Pilha
    		Elem topo(); 
 		int size();		
 };
+
+
+Pilha::Pilha(){
+  this->topoPilha = -1;
+}
+
+Pilha::~Pilha(){};
+
+bool Pilha::isEmpty( ) {
+  if (this->topoPilha == -1)
+   	return true;
+  else
+   	return false;
+}
+
+bool Pilha::isFull( ){
+  if (this->topoPilha == TAM_PILHA-1)
+   	return true;
+  else
+   	return false;
+}
+
+void Pilha::push( Elem e ){
+  if (! this->isFull( ))
+    this->e[++this->topoPilha] = e;
+  else 
+    cout << "overflow - Estouro de Pilha";
+}
+
+Elem Pilha::pop( ){
+  if (! this->isEmpty( ))
+   return this->e[this->topoPilha--];
+  else{
+    return -1;
+  }
+}
+
+Elem Pilha::topo( ){
+  if ( ! this->isEmpty( ))
+   	return this->e[this->topoPilha];
+  else{
+    cout << "underflow - Esvaziamento de Pilha";
+    return -1;
+  }
+}
+
+int Pilha::size( ){
+  return topoPilha+1;
+}
+
+
+
 #endif // #ifndef PILHA_H
 
 
