41fa3ae09073da05ddf3beedcf3140ac37b74926
brianbracco <100450308+brianbracco@users.noreply.github.com>
2022-03-28 17:32:42 -0300

    Update ProjetoFinal.cpp

--- Estrutura-De-Dados-P1/projeto-ATUAL/src/ProjetoFinal.cpp
+++ Estrutura-De-Dados-P1/projeto-ATUAL/src/ProjetoFinal.cpp
@@ -12,111 +12,58 @@ int main()
 {  
 	string notacaoInfixa, notacaoPosfixa;  
 
-	cout << " Digite a expressao infixa: ";  
+	cout << " Digite a expressao infixa: (COM LETRAS MAIUSCULAS) ";  
 	cin >> notacaoInfixa;  
-	
-	notacaoPosfixa = infixaParaPosfixa(notacaoInfixa);
-	  
-	cout << "\n Sua notacao infixa eh: " << notacaoInfixa << endl;  
-	cout << "\n Notacao convertida para Posfixa : " << notacaoPosfixa;  
-	
-	cout << endl << endl;
-	
-	string a, b, c, d, e, f, g, h, i, j , k, l, m, n , o, p, q, r, s ,t, u, v, x, w, y, z;
 
-	string str =  notacaoPosfixa;
+	notacaoPosfixa = Calculadora::InfixaParaPosfixa(notacaoInfixa);
 
-	int tamanhoStr = str.length();
-		
-	if (str.find('a') <= tamanhoStr ) { cout << "Qual o valor da variavel 'a' ?"; cin >> a; }
-	if (str.find('b') <= tamanhoStr ) { cout << "Qual o valor da variavel 'b' ?"; cin >> b; }
-	if (str.find('c') <= tamanhoStr ) { cout << "Qual o valor da variavel 'c' ?"; cin >> c; } 
-	if (str.find('d') <= tamanhoStr ) { cout << "Qual o valor da variavel 'd' ?"; cin >> d; } 
-	if (str.find('e') <= tamanhoStr ) { cout << "Qual o valor da variavel 'e' ?"; cin >> e; } 
-	if (str.find('f') <= tamanhoStr ) { cout << "Qual o valor da variavel 'f' ?"; cin >> f; } 
-	if (str.find('g') <= tamanhoStr ) { cout << "Qual o valor da variavel 'g' ?"; cin >> g; } 
-	if (str.find('h') <= tamanhoStr ) { cout << "Qual o valor da variavel 'h' ?"; cin >> h; } 
-	if (str.find('i') <= tamanhoStr ) { cout << "Qual o valor da variavel 'i' ?"; cin >> i; } 
-	if (str.find('j') <= tamanhoStr ) { cout << "Qual o valor da variavel 'j' ?"; cin >> j; } 
-	if (str.find('k') <= tamanhoStr ) { cout << "Qual o valor da variavel 'k' ?"; cin >> k; } 
-	if (str.find('l') <= tamanhoStr ) { cout << "Qual o valor da variavel 'l' ?"; cin >> l; } 
-	if (str.find('m') <= tamanhoStr ) { cout << "Qual o valor da variavel 'm' ?"; cin >> m; } 
-	if (str.find('n') <= tamanhoStr ) { cout << "Qual o valor da variavel 'n' ?"; cin >> n; } 
-	if (str.find('o') <= tamanhoStr ) { cout << "Qual o valor da variavel 'o' ?"; cin >> o; } 
-	if (str.find('p') <= tamanhoStr ) { cout << "Qual o valor da variavel 'p' ?"; cin >> p; } 
-	if (str.find('q') <= tamanhoStr ) { cout << "Qual o valor da variavel 'q' ?"; cin >> q; } 
-	if (str.find('r') <= tamanhoStr ) { cout << "Qual o valor da variavel 'r' ?"; cin >> r; } 
-	if (str.find('s') <= tamanhoStr ) { cout << "Qual o valor da variavel 's' ?"; cin >> s; } 
-	if (str.find('t') <= tamanhoStr ) { cout << "Qual o valor da variavel 't' ?"; cin >> t; } 
-	if (str.find('u') <= tamanhoStr ) { cout << "Qual o valor da variavel 'u' ?"; cin >> u; } 
-	if (str.find('v') <= tamanhoStr ) { cout << "Qual o valor da variavel 'v' ?"; cin >> v; } 
-	if (str.find('x') <= tamanhoStr ) { cout << "Qual o valor da variavel 'x' ?"; cin >> x; } 
-	if (str.find('w') <= tamanhoStr ) { cout << "Qual o valor da variavel 'w' ?"; cin >> w; } 
-	if (str.find('y') <= tamanhoStr ) { cout << "Qual o valor da variavel 'y' ?"; cin >> y; } 
-	if (str.find('z') <= tamanhoStr ) { cout << "Qual o valor da variavel 'z' ?"; cin >> z; } 
+	cout << "\n Sua notacao infixa eh: " << notacaoInfixa;  
+	cout << "\n notacao posfixa convertida: " << notacaoPosfixa;  
 
-	
-	string strArr[tamanhoStr];
-	
-	for (int cont = 0; cont < str.length(); cont++){
-		
-		if (str[cont] == '+' ) { strArr[cont] = '+'; } 
-		if (str[cont] == '-' ) { strArr[cont] = '-'; } 
-		if (str[cont] == '*' ) { strArr[cont] = '*'; } 
-		if (str[cont] == '/' ) { strArr[cont] = '/'; } 
-		if (str[cont] == '^' ) { strArr[cont] = '^'; } 
-		
-		if (str[cont] == 'a' ) { strArr[cont] = a; } 
-		if (str[cont] == 'b' ) { strArr[cont] = b; } 
-		if (str[cont] == 'c' ) { strArr[cont] = c; } 
-		if (str[cont] == 'd' ) { strArr[cont] = d; } 
-		if (str[cont] == 'e' ) { strArr[cont] = e; } 
-		if (str[cont] == 'f' ) { strArr[cont] = f; } 
-		if (str[cont] == 'g' ) { strArr[cont] = g; } 
-		if (str[cont] == 'h' ) { strArr[cont] = h; } 
-		if (str[cont] == 'i' ) { strArr[cont] = i; } 
-		if (str[cont] == 'j' ) { strArr[cont] = j; } 
-		if (str[cont] == 'k' ) { strArr[cont] = k; } 
-		if (str[cont] == 'l' ) { strArr[cont] = l; } 
-		if (str[cont] == 'm' ) { strArr[cont] = m; } 
-		if (str[cont] == 'n' ) { strArr[cont] = n; } 
-		if (str[cont] == 'o' ) { strArr[cont] = o; } 
-		if (str[cont] == 'p' ) { strArr[cont] = p; } 
-		if (str[cont] == 'q' ) { strArr[cont] = q; } 
-		if (str[cont] == 'r' ) { strArr[cont] = r; } 
-		if (str[cont] == 's' ) { strArr[cont] = s; } 
-		if (str[cont] == 't' ) { strArr[cont] = t; } 
-		if (str[cont] == 'u' ) { strArr[cont] = u; } 
-		if (str[cont] == 'v' ) { strArr[cont] = v; } 
-		if (str[cont] == 'x' ) { strArr[cont] = x; } 
-		if (str[cont] == 'w' ) { strArr[cont] = w; } 
-		if (str[cont] == 'y' ) { strArr[cont] = y; } 
-		if (str[cont] == 'z' ) { strArr[cont] = z; } 
-
-	
-	}
-	
 	cout << endl << endl << endl;
-	
-	string expressaoFinal = "";
-	string x1 = "";
-	
-	for (int xi = 0; xi < str.length(); xi++) {
-		
-		x1 = strArr[xi];
-		expressaoFinal = expressaoFinal + x1;
 
-		
+	// string str =  "A+(B*C-(D/E-G)*G)*H";
+	string str =  notacaoInfixa;
+
+	int tamanhoStr = str.length(); // https://www.cplusplus.com/reference/string/string/length/
+
+	char alfabeto[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+						'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+	FilaCircular letrasPresentes = FilaCircular();
+	for (int i = 0; i < 26; i++)
+	{
+		char letra = alfabeto[i];
+		string valorLetra;
+		if (str.find(letra) <= tamanhoStr) 
+		{
+			cout << "Qual o valor da variavel '" << letra << "'? "; cin >> valorLetra;
+			CHAVE_VALOR chaveValor = {letra, valorLetra};
+			letrasPresentes.enqueue(chaveValor);
+		}
+	};
+
+	for (int i = 0; i < letrasPresentes.size(); i++)
+	{
+		CHAVE_VALOR chaveValor = letrasPresentes.dequeue();
+		cout << chaveValor.letra << " = " << chaveValor.valor << endl;
+		letrasPresentes.enqueue(chaveValor);
 	}
 
-    cout<<"RESULTADO:  " << expressaoFinal << " = ";
-    
-    cout << calculadoraPosFixa(expressaoFinal);
-    
+	/*na expressao exemplo: A+(B*C-(D/E-G)*H)*H s� existe valores at� H, por isso s� printei at� H*/
+
+
+	/*MODIFICAR O QUE FALTA AQUI*/	
+
+	/* string expressaoPosFixa DEVE SER FEITA DE ACORDO COM VALORES PASSADOS PELO USARIO DE CADA LETRA*/
+    string expressaoPosFixa = "123*45/6-7*-8*+";
+
+    cout << "RESULTADO (ainda nao eh o correto): ";
+    cout << Calculadora::CalculadoraPosFixa(expressaoPosFixa);
+
 	cout << endl ;
-	
 	return 0;  
-	
+
 }
 
 /*
